c4f7d04abcd9a1df0dc604bca2e0d4a6812d02ac
faultroll <boxer1992@qq.com>
2022-01-31 14:00:34 +0800

    [CHG] fix compile error

--- gettid.c
+++ gettid.c
@@ -1,7 +1,7 @@
 
-#include "cthread.h"
-#include "catomic.h"
-#include "ctime.h"
+#include "thread_c.h"
+#include "atomic_c.h"
+#include "time_c.h"
 
 // TODO try to support multi |memory_tracker|
 // eg. thread a may have tid_ 1 in tracker a and tid_ 3 in tracker b
